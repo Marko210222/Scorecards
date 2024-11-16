3a7533380aa8738e5d8bac6ddea82874cdf50eb4
Marko210222 <mark.zuckerberg32800@gmail.com>
2024-11-16 10:04:01 +0100

    index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["🌟 Watching Scorecards", "💳 Creating Scorecards!"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
